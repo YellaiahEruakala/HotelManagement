1c22f40728cd2a8f7e452882231f37be9ca2ec75
YellaiahEruakala <78197751+YellaiahEruakala@users.noreply.github.com>
2021-07-23 00:39:38 +0530

    commit

--- HotelManagement/settings.py
+++ HotelManagement/settings.py
@@ -131,4 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
