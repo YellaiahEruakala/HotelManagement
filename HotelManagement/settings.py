fb305866e3fb24f2fe71c9d1a912e457b9e2542e
YellaiahEruakala <78197751+YellaiahEruakala@users.noreply.github.com>
2021-07-22 23:20:56 +0530

    Update settings.py

--- HotelManagement/settings.py
+++ HotelManagement/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-# from pathlib import path
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&14a!_x1ta9vd34fg8eh)!ig^mat=o-2)_t2(g*7!!zv2mv8x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'capicocoffeeshop.herokuapp.com']
 
