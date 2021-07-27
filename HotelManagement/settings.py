17714201a575ddbea74be00c83d6c72ec6b63841
YellaiahEruakala <78197751+YellaiahEruakala@users.noreply.github.com>
2021-07-27 18:57:57 +0530

    Update settings.py

--- HotelManagement/settings.py
+++ HotelManagement/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'HotelManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'HotelManagement',
-#        'USER':'postgres',
-#        'PASSWORD':'123456',
-#        'HOST':'localhost',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HotelManagement',
+        'USER':'postgres',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 
